732c928e61b4e2ac40ff7609ae7c191b6f1e9ed8
Mr.Ye <Owen718@users.noreply.github.com>
2020-06-29 14:36:02 +0800

    v0.1

---
+++ fast特征检测.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread(r'C:\Users\Owen\Pictures\image.jpg')
+# Initiate FAST object with default values
+fast = cv2.FastFeatureDetector_create()
+# find and draw the keypoints
+kp = fast.detect(img,None)
+img2 = cv2.drawKeypoints(img, kp, None, color=(255,0,0))
+# Print all default params
+print( "Threshold: {}".format(fast.getThreshold()) )
+print( "nonmaxSuppression:{}".format(fast.getNonmaxSuppression()) )
+print( "neighborhood: {}".format(fast.getType()) )
+print( "Total Keypoints with nonmaxSuppression: {}".format(len(kp)) )
+cv2.imshow('fast_true',img2)
+# Disable nonmaxSuppression
+fast.setNonmaxSuppression(0)
+kp = fast.detect(img,None)
+print( "Total Keypoints without nonmaxSuppression: {}".format(len(kp)) )
+img3 = cv2.drawKeypoints(img, kp, None, color=(255,0,0))
+cv2.imshow('fast_false',img3)
+cv2.waitKey()
